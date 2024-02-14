cc26d8c08e81ca8989663bcd00947dbb53b92196
PrakashML <prakashkrishnan08@gmail.com>
2024-02-14 23:08:59 +0530

    Successfully Created A Contact

--- src/com/contacts/book/Main.java
+++ src/com/contacts/book/Main.java
@@ -3,5 +3,8 @@ package com.contacts.book;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome To AddressBook");
+        Contact person = new Contact("Prakash","k","lake view road","chennai","tn",456464,657676769,"pk4652@gmail.com");
+        System.out.println(person.getFirstName());
+        System.out.println(person.getLastName());
     }
 }
